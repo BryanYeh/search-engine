3ebe79e4a70135679795101c9a57fe53413af963
Bryan Y <cs337bryan@gmail.com>
2015-01-31 17:38:53 -0800

    changed stuff

--- src/main/java/cs454/searchengine/search_engine/App.java
+++ src/main/java/cs454/searchengine/search_engine/App.java
@@ -2,7 +2,7 @@ package cs454.searchengine.search_engine;
 
 /**
  * http://www.java2s.com/Tutorial/Java/0320__Network/Getallhyperlinksfromawebpage.htm
-**/
+ **/
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,24 +15,20 @@ import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.parser.ParserDelegator;
 
 public class App {
-  public static void main(String args[]) throws Exception {
-    URL url = new URL(args[0]);
-    Reader reader = new InputStreamReader((InputStream) url.getContent());
-    System.out.println("<HTML><HEAD><TITLE>Links for " + args[0] + "</TITLE>");
-    System.out.println("<BASE HREF=\"" + args[0] + "\"></HEAD>");
-    System.out.println("<BODY>");
-    new ParserDelegator().parse(reader, new LinkPage(), true);
-    System.out.println("</BODY></HTML>");
-  }
+	public static void main(String args[]) throws Exception {
+		URL url = new URL(args[0]);
+		Reader reader = new InputStreamReader((InputStream) url.getContent());
+		new ParserDelegator().parse(reader, new LinkPage(), true);
+	}
 }
 
 class LinkPage extends HTMLEditorKit.ParserCallback {
 
-  public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
-    if (t == HTML.Tag.A) {
-      System.out.println("<A HREF=\"" + a.getAttribute(HTML.Attribute.HREF) + "\">"
-          + a.getAttribute(HTML.Attribute.HREF) + "</A><BR>");
-    }
-  }
+	public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+		if (t == HTML.Tag.A) {
+			if (!((String) a.getAttribute(HTML.Attribute.HREF)).startsWith("#"))
+				System.out.println(a.getAttribute(HTML.Attribute.HREF));
+		}
+	}
 
 }
